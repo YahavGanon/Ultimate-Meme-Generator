f77e867fe0a9042a509ac1f293293e16dd7cb19e
YahavGanon <yahavganon86@gmail.com>
2024-02-28 14:25:40 +0200

    Create a canvas added photos and some css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 'use strict'
+
+var gImgs = [
+    {id: 1, url: 'img/1.jpg', keywords: ['funny', 'president', 'fat']},
+    {id: 2, url: 'img/2.jpg', keywords: ['animal', 'dogs', 'love']},
+    {id: 3, url: 'img/3.jpg', keywords: ['child', 'dogs', 'love']},
+    {id: 4, url: 'img/4.jpg', keywords: ['cat', 'dogs', 'funny']},
+    {id: 5, url: 'img/5.jpg', keywords: ['child', 'sea', 'funny']},
+    {id: 6, url: 'img/6.jpg', keywords: ['person', 'smart', 'explain']},
+    {id: 7, url: 'img/7.jpg', keywords: ['child', 'funny', 'eyes']},
+    {id: 8, url: 'img/8.jpg', keywords: ['smart', 'person', 'eyes']},
+    {id: 9, url: 'img/9.jpg', keywords: ['child', 'person', 'smart']},
+    {id: 10, url: 'img/10.jpg', keywords: ['smart', 'person', 'president']},
+    {id: 11, url: 'img/11.jpg', keywords: ['sport', 'love', 'funny']},
+    {id: 12, url: 'img/12.jpg', keywords: ['funny', 'person', 'check']},
+    {id: 13, url: 'img/13.jpg', keywords: ['funny', 'cheers', 'person']},
+    {id: 14, url: 'img/14.jpg', keywords: ['person', 'metrix', 'sunglass']},
+    {id: 15, url: 'img/15.jpg', keywords: ['person', 'smart', 'eyes']},
+    {id: 16, url: 'img/16.jpg', keywords: ['person', 'smart', 'funny']},
+    {id: 17, url: 'img/17.jpg', keywords: ['person', 'president', 'funny']},
+    {id: 18, url: 'img/18.jpg', keywords: ['person', 'toy', 'funny']},
+]
